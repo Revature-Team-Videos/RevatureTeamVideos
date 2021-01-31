74e5651689e20fd0b04a9a026088f8c80c9641d9
zach sanders <zach.sanders@revature.net>
2021-01-30 20:53:51 -0500

    add user functionality

--- aspnet/VideoShare.Client/Controllers/UserController.cs
+++ aspnet/VideoShare.Client/Controllers/UserController.cs
@@ -23,31 +23,36 @@ namespace VideoShare.Client.Controllers
         [HttpPost("/CreateAccount")]
         public async Task<IActionResult> CreateAccount(string username, string email)
         {
-            var response = await _http.GetAsync(apiUrl);
+            var responseurl = apiUrl + "/users/add";
             UserViewModel userview = new UserViewModel();
             userview.Email = email;
             userview.Username = username;
-            // create repo method for new user
-            _repo.Save();
+            var response = await _http.PostAsync(responseurl, userview);
+            if (response.IsSuccessStatusCode)
+            {
+                return View("logon", userview);
+            }
+            return View ("error");
 
-            return View("logon", userview);
         }
         [HttpPost("/Login")]
         public async Task<IActionResult> Login(string Username)
         {
             UserViewModel userview = new UserViewModel(); 
-            var response = await _http.GetAsync(apiUrl + "/users/" + Username);
+            var response = await _http.GetAsync(apiUrl + "/users/username/" + Username);
             if (response.IsSuccessStatusCode)
             {
                 var content = JsonSerializer.Deserialize<UserViewModel> (await response.Content.ReadAsStringAsync());
                 userview.Username = content.Username;
                 userview.Email = content.Email;
+                TempData.Put<UserViewModel>("userview", userview);
+
                 return View("logonsuccess", userview);
             }
             else return View("logonerror");
         }
         [HttpGet("/NewRoom")]
-        public async Task<IActionResult> CreateRoom(string username, string videourl)
+        public async Task<IActionResult> CreateRoom(string videourl)
         {
             var response = await _http.GetAsync(apiUrl + "/room");
             if (response.IsSuccessStatusCode)
@@ -62,15 +67,19 @@ namespace VideoShare.Client.Controllers
         [HttpPost("/room")]
         public async Task<IActionResult> JoinRoom(string roomid)
         {
-            var response = await _http.GetAsync(apiUrl + "/room/" + roomid);
+            var response = await _http.GetAsync(apiUrl + "/rooms/" + roomid);
             if (response.IsSuccessStatusCode)
             {
                 var content = JsonSerializer.Deserialize<RoomViewModel> (await response.Content.ReadAsStringAsync());
                 RoomViewModel roomview = new RoomViewModel();
                 roomview.RoomId = roomid;
-                roomview.Party.Add(userview);
-
-                return View("Room");
+                UserViewModel userview = TempData.Get<UserViewModel>("userview");
+                var response2 = await _http.PostAsync(apiUrl + "/rooms/" + roomid + "/" + userview.Username, userview);
+                if (response2.IsSuccessStatusCode)
+                {
+                    return View ("room");
+                }
+                return View("Error");
             }
             else return View("error");
             
@@ -108,7 +117,7 @@ namespace VideoShare.Client.Controllers
         [HttpGet("/user")]
         public async Task<IActionResult> GetProfile(string username)
         {
-            var response = await _http.GetAsync(apiUrl +"/user" + username);
+            var response = await _http.GetAsync(apiUrl +"/users/username/" + username);
             if (response.IsSuccessStatusCode)
             {
                 var content = JsonSerializer.Deserialize<UserViewModel> (await response.Content.ReadAsStringAsync());
