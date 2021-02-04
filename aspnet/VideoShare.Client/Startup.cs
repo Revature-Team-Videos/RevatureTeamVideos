82562cadddc2589a546910b32a86f565f2cf4112
jim phan <phanjim2@hotmail.com>
2021-02-03 22:40:28 -0800

    Fixed one Azure

--- aspnet/VideoShare.Client/Startup.cs
+++ aspnet/VideoShare.Client/Startup.cs
@@ -54,7 +54,8 @@ namespace VideoShare.Client
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
